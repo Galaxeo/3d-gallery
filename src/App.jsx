a4e51cf424f6b7c82f0610e8f11e80ed67bad7c5
Galaxeo <hwjustincheok@gmail.com>
2024-01-24 21:19:52 -0800

    Start over

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Screen from "./Screen";
-import Background from "./Background";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return (
-    <>
-      <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 100, fov: 60 }}>
-        <Screen></Screen>
-        <Background></Background>
-      </Canvas>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/Keyboards.jsx
+++
@@ -1,5 +0,0 @@
-import * as THREE from "three";
-import { useEffect, useState, useRef } from "react";
-import { useFrame, useThree } from "@react-three/fiber";
-import { Image, Text } from "@react-three/drei";
-import "./App.css";
